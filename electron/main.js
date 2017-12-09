ad24ff30c05d6ff87cb871aaf9971012ebbf7d8e
Thalles Batista <thallesmweb@gmail.com>
2017-12-08 22:54:37 -0200

    Delete main.js

--- electron/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var electron_1 = require("electron");
-var win = null;
-electron_1.app.on('ready', function () {
-    win = new electron_1.BrowserWindow();
-    win.loadURL("file://" + __dirname + "/../proj/index.html");
-    win.openDevTools();
-});
